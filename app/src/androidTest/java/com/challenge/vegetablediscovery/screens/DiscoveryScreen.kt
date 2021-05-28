aa5fbccb17869d7291995a069d0207b8d036ed0f
curlx <ninja075@gmail.com>
2021-05-28 21:14:58 +0700

    add a UI automation test

---
+++ app/src/androidTest/java/com/challenge/vegetablediscovery/screens/DiscoveryScreen.kt
@@ -0,0 +1,23 @@
+package com.challenge.vegetablediscovery.screens
+
+import android.view.View
+import com.agoda.kakao.image.KImageView
+import com.agoda.kakao.recycler.KRecyclerItem
+import com.agoda.kakao.recycler.KRecyclerView
+import com.agoda.kakao.screen.Screen
+import com.agoda.kakao.text.KTextView
+import com.challenge.vegetablediscovery.R
+import org.hamcrest.Matcher
+
+class DiscoveryScreen: Screen<DiscoveryScreen>() {
+
+    val vegetableList: KRecyclerView = KRecyclerView(
+        builder = { withId(R.id.vegetable_list) },
+        itemTypeBuilder = { itemType(::MainItem) }
+    )
+
+    class MainItem(parent: Matcher<View>) : KRecyclerItem<MainItem>(parent) {
+        val image: KImageView = KImageView(parent) { withId(R.id.image) }
+        val name: KTextView = KTextView(parent) { withId(R.id.name) }
+    }
+}
---
+++ app/src/androidTest/java/com/challenge/vegetablediscovery/screens/VegetableDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.challenge.vegetablediscovery.screens
+
+import com.agoda.kakao.image.KImageView
+import com.agoda.kakao.screen.Screen
+import com.agoda.kakao.text.KTextView
+import com.challenge.vegetablediscovery.R
+
+class VegetableDetailScreen: Screen<VegetableDetailScreen>() {
+
+    val image: KImageView = KImageView { withId(R.id.image) }
+    val name: KTextView = KTextView { withId(R.id.name) }
+    val description: KTextView = KTextView { withId(R.id.description) }
+    val closeButton: KImageView = KImageView { withId(R.id.close_button) }
+}
