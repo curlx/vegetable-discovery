6b29d619042fdfd4fc7815905e445a306c4c25cd
curlx <ninja075@gmail.com>
2021-05-30 23:13:35 +0700

    fix actual and expected position

--- app/src/test/java/com/challenge/vegetablediscovery/base/BasicAssertions.kt
+++ app/src/test/java/com/challenge/vegetablediscovery/base/BasicAssertions.kt
@@ -2,5 +2,5 @@ package com.challenge.vegetablediscovery.base
 
 import com.google.common.truth.Truth.assertThat
 
-infix fun <T> T.shouldEqual(expected: T?): T = this.apply { assertThat(expected).isEqualTo(this) }
-infix fun <T> T.shouldNotEqual(expected: T?): T = this.apply { assertThat(expected).isNotEqualTo(this) }
+infix fun <T> T.shouldEqual(expected: T?): T = this.apply { assertThat(this).isEqualTo(expected) }
+infix fun <T> T.shouldNotEqual(expected: T?): T = this.apply { assertThat(this).isNotEqualTo(expected) }
