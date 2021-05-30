c201c9486011f99d538f71a2ffefae624e2b9bfb
curlx <ninja075@gmail.com>
2021-05-30 16:55:57 +0700

    remove unused resources

--- app/src/main/java/com/challenge/vegetablediscovery/MainActivity.kt
+++ app/src/main/java/com/challenge/vegetablediscovery/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.challenge.vegetablediscovery
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.challenge.vegetablediscovery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,20 +13,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_discovery, R.id.navigation_dashboard
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        navView.isVisible = false
         supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/challenge/vegetablediscovery/ui/vegetablelist/VegetableListFragment.kt
+++ app/src/main/java/com/challenge/vegetablediscovery/ui/vegetablelist/VegetableListFragment.kt
@@ -1,4 +1,4 @@
-package com.challenge.vegetablediscovery.ui.discovery
+package com.challenge.vegetablediscovery.ui.vegetablelist
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,21 +13,17 @@ import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.challenge.vegetablediscovery.R
-import com.challenge.vegetablediscovery.databinding.FragmentDiscoveryBinding
-import com.challenge.vegetablediscovery.ui.vegetablelist.VegetableListAdapter
-import com.challenge.vegetablediscovery.ui.discovery.viewmodel.DiscoveryViewModel
-import com.challenge.vegetablediscovery.ui.vegetablelist.VegetableListViewModel
+import com.challenge.vegetablediscovery.databinding.FragmentVegetableListBinding
 import com.challenge.vegetablediscovery.ui.vitaminfilter.VitaminFilterListAdapter
 import com.challenge.vegetablediscovery.ui.vitaminfilter.VitaminFilterViewModel
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DiscoveryFragment : Fragment(), VegetableListAdapter.Listener, SwipeRefreshLayout.OnRefreshListener {
+class VegetableListFragment : Fragment(), VegetableListAdapter.Listener, SwipeRefreshLayout.OnRefreshListener {
 
-    private lateinit var discoveryViewModel: DiscoveryViewModel
     private val vegetableListViewModel: VegetableListViewModel by viewModel<VegetableListViewModel>()
     private val vitaminFilterViewModel: VitaminFilterViewModel by viewModel<VitaminFilterViewModel>()
-    private var _binding: FragmentDiscoveryBinding? = null
+    private var _binding: FragmentVegetableListBinding? = null
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -38,9 +33,7 @@ class DiscoveryFragment : Fragment(), VegetableListAdapter.Listener, SwipeRefres
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        discoveryViewModel = ViewModelProvider(this).get(DiscoveryViewModel::class.java)
-
-        _binding = FragmentDiscoveryBinding.inflate(inflater, container, false)
+        _binding = FragmentVegetableListBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -78,7 +71,7 @@ class DiscoveryFragment : Fragment(), VegetableListAdapter.Listener, SwipeRefres
     }
 
     override fun onVegetableItemClick(vegetableId: Long) {
-        val direction = DiscoveryFragmentDirections.actionNavigationDiscoveryToNavigationVegetableDetail(vegetableId)
+        val direction = VegetableListFragmentDirections.actionNavigationDiscoveryToNavigationVegetableDetail(vegetableId)
         findNavController().navigate(direction)
     }
 
